a336b26ca528a8f2fd15973ece0ab87eb8c3108f
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-02-18 16:14:26 +0600

    Deposit and balance interaction

--- js/banking.js
+++ js/banking.js
@@ -2,15 +2,41 @@
 function getInputValue(inputs) {
     const inputField = document.getElementById(inputs);
     const value = parseFloat(inputField.value);
+    inputField.value = '';
     return value;
 
 }
 
+// UpdateTotal
+function UpdateTotal(fieldId, amount) {
+    const previousTotal = document.getElementById(fieldId);
+    const getTotal = parseFloat(previousTotal.innerText);
+    const newTotal = getTotal + amount;
+    previousTotal.innerText = newTotal;
+}
 
-// Deposit added 
+// Balance Update 
+function balanceUpdate(amount) {
+    const balance = document.getElementById('balance-total');
+    const balanceInText = parseFloat(balance.innerText);
+    const newBalance = balanceInText + amount;
+    balance.innerText = newBalance;
+}
+
+//Handle  Deposit 
 document.getElementById('deposit-button').addEventListener('click',
     function () {
         const amount = getInputValue('deposit-input');
-        console.log(amount);
+        UpdateTotal('total-deposit', amount);
+        balanceUpdate(amount);
+    }
+)
+
+// Handle  Withdraw 
+document.getElementById('withdraw-button').addEventListener('click',
+    function () {
+        const amount = getInputValue('withdraw-input');
+        UpdateTotal('withdraw-text', amount);
+
     }
 )
